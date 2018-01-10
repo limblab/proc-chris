2eddc026600babcb234939f3e8169f246138d89e
Christopher Steven Versteeg <csv057@fsm.northwestern.edu>
2018-01-10 13:25:40 -0600

    Adding support for OOR into file loading

--- lib/getTask.m
+++ lib/getTask.m
@@ -5,6 +5,8 @@ function [ cdsTask ] = getTask( inTask )
         cdsTask = 'CObump';
     elseif strcmp(inTask(1:2),'RW')
         cdsTask = 'RW';
+    elseif strcmp(inTask(1:3), 'OOR')
+        cdsTask = 'OOR';
     else
         cdsTask = 'Unknown';
     end
